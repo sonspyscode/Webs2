397af8272a0bab26952ae5254c460964dbea3675
sonspyscode <79440527+sonspyscode@users.noreply.github.com>
2024-05-24 03:52:21 +0800

    Update server.js

--- server.js
+++ server.js
@@ -4,14 +4,21 @@ const path = require('path');
 
 const app = express();
 
-// Menggunakan middleware compression untuk mengaktifkan kompresi gzip
+// Activate gzip compression
 app.use(compression());
 
-// Menetapkan direktori statis
-app.use(express.static(path.join('Webs2', 'public')));
+// Set the Cache-Control header for static files
+app.use(express.static(path.join(__dirname, 'public'), {
+  maxAge: '1y',
+  setHeaders: (res, path) => {
+    if (path.endsWith('.css') || path.endsWith('.js') || path.endsWith('.jpg') || path.endsWith('.png') ) {
+      res.setHeader('Cache-Control', 'public, max-age=2592000, immutable');
+    }
+  }
+}));
 
-// // Jika dijalankan diluar Github Pages, Jalankan server pada port 3000
-// const PORT = 3000;
-// app.listen(PORT, () => {
-//     console.log(`Server running on port ${PORT}`);
+// Start the server
+const PORT = 5500;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
